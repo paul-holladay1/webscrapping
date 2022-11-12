c941cd5007090968447396bfd82b2e25629f003c
Paul <paul_holladay1@baylor.edu>
2022-11-12 02:02:00 -0600

    almost done

--- webscraping-project.py
+++ webscraping-project.py
@@ -26,15 +26,31 @@ for row in tablecells[1:6]:
     #     img = row.findAll('img')
     #     icon = img[0].
 
-    for price in tablecells:
-        span = row.findAll('span')
-        price = float(span[0].text.replace('$','').replace(',',''))
+        for price in tablecells:
+            span = row.findAll('span')
+            price = float(span[3].text.replace('$','').replace(',',''))
+            
+        for per_change in tablecells:
+            span = row.findAll('span')
+            per_change = float(span[6].text.replace(',','').replace('%',''))
+    
+        # for carrot in tablecells():
+        for carrot in row.findAll('span', class_ = "icon-Caret-up" and "icon-Caret-down"):
+            # span = row.findAll('span', class_ = "icon-Caret-up" and "icon-Caret-down")
+            if carrot == "icon-Caret-up":
+                # diff = price * per_change
+                # original_price = price - diff
+                x = "up"
+            if carrot == "icon-Caret-down":
+                # diff = price * per_change
+                # original_price = price - diff
+                x = "down"
 
     
-    
-    print(rank)
-    print(name)
-    print(price)
-    # print(icon)
-    # print(str(price))
+    print(f'Rank: {rank}')
+    print(f'Name: {name}')
+    print(f'Price: ${price:,.2f}')
+    print(f'% Change in Last 24hrs: {per_change}%')
+    print(f'Status: {x}')
     print()
+
